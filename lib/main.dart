2c1f5302e24d119e9204bee32161a31aced42f9e
Virgilio F Tuga Jr <vftugajr@nemsu.edu.ph>
2025-02-06 15:58:57 +0800

    Second Topic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,53 +12,72 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromRGBO(23,53,0, 1),
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(23,53,0, 1),
-          title: Row(
-            children: [
-              Icon(Icons.settings_accessibility_rounded,
-              color: Colors.white,
-              size: 40,
-              ),
-              SizedBox(width: 15,),
-              Text("Finto", 
-                style: TextStyle(
-                  fontSize: 45,
-                  color: Colors.white,
-                  fontWeight: FontWeight.w800
-                ) ,),
-            ],
-          ),
-        ),
-        endDrawer: Drawer(
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 20, top: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text("All In One App\nFinance for\nYour Business",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 55,
-                fontWeight: FontWeight.w500
-              )),
-              SizedBox(height: 50,),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Center(child: const Text('Explore')),
+            actions: [
               Padding(
                 padding: const EdgeInsets.only(right: 20),
-                child: Text("With Finto, you get a clean and straightforward app that helps you keep track of your spending, save money, and get smart tips on managing your finances.",
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18
-                )),
-              ),
+                child: Icon(Icons.more_horiz),
+              )
             ],
           ),
-        ),
-      ),
+          drawer: Drawer(
+            backgroundColor: Colors.amber[700],
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(15),
+            child: Container(
+              padding: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.black,
+                border: Border.all(
+                  width: 5.0,
+                ),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              width: 400,
+              height: 200,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Icon(Icons.person,
+                          size: 60,
+                          color: Colors.white,),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Chaya Lindsey",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold
+                            ),),
+                            Text("UI/UX Designer",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                            ),),
+                          ],
+                          
+                        ),
+                        SizedBox(width: 100,),
+                        Icon(Icons.bookmark,
+                        color: Colors.white,)
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
-
