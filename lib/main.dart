9d2f460d85a52c05d08f8652daefa48d85c6612e
Virgilio F Tuga Jr <vftugajr@nemsu.edu.ph>
2025-02-10 15:58:18 +0800

    Third topic

--- lib/main.dart
+++ lib/main.dart
@@ -6,78 +6,66 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            title: Center(child: const Text('Explore')),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 20),
-                child: Icon(Icons.more_horiz),
-              )
-            ],
-          ),
-          drawer: Drawer(
-            backgroundColor: Colors.amber[700],
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Container(
-              padding: EdgeInsets.all(5),
-              decoration: BoxDecoration(
-                color: Colors.black,
-                border: Border.all(
-                  width: 5.0,
-                ),
-                borderRadius: BorderRadius.circular(20)
-              ),
-              width: 400,
-              height: 200,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 10),
-                child: Column(
+        appBar: AppBar(
+          backgroundColor: Colors.amber[800],
+          title: Center(child: Text("Activity 1")),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 20),
+              child: Icon(Icons.logout),
+            )
+          ],
+        ),
+        drawer: Drawer(
+          backgroundColor: Colors.amber[800],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 75),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Row(
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Icon(Icons.person,
-                          size: 60,
-                          color: Colors.white,),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("Chaya Lindsey",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold
-                            ),),
-                            Text("UI/UX Designer",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 16,
-                            ),),
-                          ],
-                          
-                        ),
-                        SizedBox(width: 100,),
-                        Icon(Icons.bookmark,
-                        color: Colors.white,)
-                      ],
-                    ),
+                    Icon(Icons.menu_book,
+                    size: 40,
+                    color: Colors.white,),
+                    SizedBox(width: 15,),
+                    Text("Classes",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white
+                    ),),
                   ],
                 ),
-              ),
+              ],
             ),
-          )),
+          ),
+        ),
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            width: 270,
+            height: 270,
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: const Color.fromRGBO(255, 143, 0, 1),
+              ),
+              borderRadius: BorderRadius.circular(30)
+            ),
+            child: Text("Name: Virgilio F. Tuga Jr.\nAge:24\nGender: Male\nCourse: BSCS",
+            style: TextStyle(
+              fontSize: 20
+            ),),
+          ),
+        ),
+      ),
     );
   }
 }
+
